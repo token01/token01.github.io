293e712d36d00a2d11e18625514308516cc432b3
Kong Ming(空明) <token01@126.com>
2022-11-21 18:50:46 +0800

    deal with the problem

---
+++ dist/assets/index.html.d95f7d78.js
@@ -0,0 +1,18 @@
+import{_ as s}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as l,c,a as e,b as a,w as r,d as t,e as d,r as o}from"./app.164de6c4.js";const h={},u=e("h2",{id:"推荐阅读",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#推荐阅读","aria-hidden":"true"},"#"),t(" 推荐阅读")],-1),_={href:"https://github.com/itwanger/codingmore-learning",target:"_blank",rel:"noopener noreferrer"},m=e("h2",{id:"参与贡献",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#参与贡献","aria-hidden":"true"},"#"),t(" 参与贡献")],-1),p=e("ol",null,[e("li",null,"如果你对本项目有任何建议或发现文中内容有误的，欢迎提交 issues 进行指正。"),e("li",null,"对于文中我没有涉及到知识点，欢迎提交 PR。")],-1);function v(f,J){const n=o("RouterLink"),i=o("ExternalLinkIcon");return l(),c("div",null,[u,e("ul",null,[e("li",null,[a(n,{to:"/xuexiluxian/"},{default:r(()=>[t("学习路线👉")]),_:1}),t(" : 一份涵盖 Java、C 语言、C++、Python、Go、操作系统、前端、数据结构与算法、蓝桥杯、大数据、Android、MySQL、.NET的全方位编程学习路线！清晰且有效！")]),e("li",null,[a(n,{to:"/sidebar/sanfene/nixi.html"},{default:r(()=>[t("面渣逆袭📗")]),_:1}),t(" ：面试前必刷，硬核理解版八股文，包括 Java 基础（JavaSE）、Java 集合框架、Java 并发编程（Java 多线程）、Java 虚拟机（JVM）、Spring、Redis、MyBatis、MySQL、操作系统、计算机网络、RocketMQ等等，助你拿到心仪 offer！")]),e("li",null,[a(n,{to:"/pdf/"},{default:r(()=>[t("Java程序员常读书单📚")]),_:1}),t("：超1000本PDF，助力Java 程序员构建最强知识体系，涵盖Java、设计模式、数据库、数据结构与算法、大数据、架构、其他编程语言等等，再也不用辛苦去找下载地址了，这里全都有。")]),e("li",null,[e("a",_,[t("编程喵喵🐱实战项目学习教程"),a(i)]),t(" ：codingmore（Spring Boot+Vue 的前后端分离项目，一款值得一试的开源知识库学习网站）的学习教程，需要项目经验的 Java 开发者必备！")])]),d(` ## 公众号
+
+最近整理了一份牛逼的学习资料，包括但不限于Java基础部分（JVM、Java集合框架、多线程），还囊括了 **数据库、计算机网络、算法与数据结构、设计模式、框架类Spring、Netty、微服务（Dubbo，消息队列） 网关** 等等等等……详情戳：[可以说是2022年全网最全的学习和找工作的PDF资源了](https://tobebetterjavaer.com/pdf/programmer-111.html)
+
+
+
+## star趋势图
+
+[![Star History Chart](https://api.star-history.com/svg?repos=itwanger/toBeBetterJavaer&type=Date)](https://star-history.com/#itwanger/toBeBetterJavaer&Date)
+
+
+## 捐赠鼓励
+
+开源不易，如果《Java 程序员进阶之路》对你有些帮助，可以请作者喝杯咖啡，算是对开源做出的一点点鼓励吧！
+
+<div align="left">
+    <img src="http://cdn.tobebetterjavaer.com/tobebetterjavaer/images/weixin-zhifu.png" width="260px">
+</div> `),m,p])}const b=s(h,[["render",v],["__file","index.html.vue"]]);export{b as default};

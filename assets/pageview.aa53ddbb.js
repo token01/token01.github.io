a9eb8d2700185982172c33498c2f7b12c6d7e02c
token01 <token01@126.com>
2022-12-09 15:38:16 +0800

    Deploying to gh-pages from @ token01/token01.github.io@54b5c394379b0334ececb724215d9f671956b5e3 🚀

--- assets/pageview.aa53ddbb.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app.1ee57811.js";const t=()=>e({serverURL:"https://waline.newzone.top"});export{t as updatePageview};
---
+++ assets/pageview.e1049806.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app.c2a49a29.js";const t=()=>e({serverURL:"https://waline.newzone.top"});export{t as updatePageview};
